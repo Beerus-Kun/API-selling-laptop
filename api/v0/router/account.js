a4f50546c3e2f2765a5bf5e253daa8263c8b2362
Beerus-Kun <67775920+Beerus-Kun@users.noreply.github.com>
2021-10-15 17:20:05 +0700

    then engine

--- api/v0/router/account.js
+++ api/v0/router/account.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 const Mailer = require('../../../mail');
